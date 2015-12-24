ec0deaee3c9f2fbf15b8a5e71873f6ec9df510cd
srinivasaltair <srnvs0708@gmail.com>
2015-12-24 21:55:15 +0530

    Styled list, show and messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = 'Article was successfully created'
+      flash[:success] = 'Article was successfully created'
       redirect_to article_path(@article)
     else
       render 'new'
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = 'Articles updated successfully!!'
+      flash[:success] = 'Articles updated successfully!!'
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -40,7 +40,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = 'Articles was successfully destroyed!!'
+    flash[:danger] = 'Articles was successfully destroyed!!'
     redirect_to articles_path
   end
 
